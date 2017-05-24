706931f4725fa8d9e8581d746bc32021b9facb84
Li Linyu <li.linyu@gmail.com>
2017-05-24 16:36:33 +0100

    Added test case to create a Product from JSON content

--- JLDemo/Product.swift
+++ JLDemo/Product.swift
@@ -20,4 +20,20 @@ struct Product {
         self.title = title
         self.image = image
     }
+    
+    init?(json: [String: Any]) {
+        guard let productId = json["productId"] as? String,
+            let priceInfo = json["price"] as? [String: Any],
+            let price = priceInfo["now"] as? String,
+            let title = json["title"] as? String,
+            let image = json["image"] as? String
+        else {
+                return nil
+        }
+        
+        self.productId = productId
+        self.price = price
+        self.title = title
+        self.image = image
+    }
 }
--- JLDemoTests/ProductsTests.swift
+++ JLDemoTests/ProductsTests.swift
@@ -31,4 +31,26 @@ class ProductsTests: XCTestCase {
         XCTAssertEqual(product.image, "//sample.com/image.jpg")
     }
     
+    func testNewProductFromJSON() {
+        let bundle = Bundle(for: ProductsTests.self)
+        guard let filePath = bundle.path(forResource: "Stub_OneProduct", ofType: "json") else {
+            XCTFail("Failed to read file Stub_OneProduct.json")
+            return
+        }
+        
+        if let jsonData = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+            let json = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [String: Any] {
+            
+            let product = Product(json:json!)!
+            
+            XCTAssertNotNil(product)
+            XCTAssertEqual(product.productId, "1913267")
+            XCTAssertEqual(product.price, "329.00")
+            XCTAssertEqual(product.title, "Bosch SMS53M02GB Freestanding Dishwasher, White")
+            XCTAssertEqual(product.image, "//johnlewis.scene7.com/is/image/JohnLewis/234326367?")
+        } else {
+            XCTFail("Failed to read file content from Stub_OneProduct.json")
+        }
+    }
+    
 }
