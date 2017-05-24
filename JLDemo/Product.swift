113d91577d596895ece780a21aba2521d8f47ba8
Li Linyu <li.linyu@gmail.com>
2017-05-24 17:37:31 +0100

    Added errors handling to Product

--- JLDemo/Product.swift
+++ JLDemo/Product.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+enum JSONSerializationError: Error {
+    case missing(String)
+    case invalid(String)
+}
+
 struct Product {
     let productId: String
     let price: String
@@ -21,14 +26,29 @@ struct Product {
         self.image = image
     }
     
-    init?(json: [String: Any]) {
-        guard let productId = json["productId"] as? String,
-            let priceInfo = json["price"] as? [String: Any],
-            let price = priceInfo["now"] as? String,
-            let title = json["title"] as? String,
-            let image = json["image"] as? String
-        else {
-                return nil
+    init?(json: [String: Any]) throws {
+        guard let productId = json["productId"] as? String else {
+            throw JSONSerializationError.missing("productId")
+        }
+        
+        guard let priceInfo = json["price"] as? [String: Any] else {
+            throw JSONSerializationError.missing("price")
+        }
+        
+        guard let price = priceInfo["now"] as? String else {
+            throw JSONSerializationError.missing("now")
+        }
+        
+        guard NumberFormatter().number(from: price) != nil else {
+            throw JSONSerializationError.invalid("now")
+        }
+        
+        guard let title = json["title"] as? String else {
+            throw JSONSerializationError.missing("title")
+        }
+        
+        guard let image = json["image"] as? String else {
+            throw JSONSerializationError.missing("image")
         }
         
         self.productId = productId
