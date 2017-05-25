58979ced506acef6ee2ce0507754ccfe5d6ebc8d
Li Linyu <li.linyu@gmail.com>
2017-05-25 15:12:37 +0100

    Tweak the title font

--- JLDemo/AppDelegate.swift
+++ JLDemo/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName : UIFont(name: "GillSans-Light", size: 22)!,
+                                                            NSForegroundColorAttributeName: UIColor.darkGray]
+        
         return true
     }
 
